3d801499c94d754ebe574b3530cd41d511a6ce3f
Karl <tekamkarl@gmail.com>
2023-11-16 21:08:39 +0100

    edit the size of the windows

--- src/main/java/com/example/drawapp/Application.java
+++ src/main/java/com/example/drawapp/Application.java
@@ -7,10 +7,13 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
+
+    public static int WIN_WIDHT = 1000;
+    public static int WIN_HEIGHT = 600;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("draw-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        Scene scene = new Scene(fxmlLoader.load(), WIN_WIDHT, WIN_HEIGHT);
         stage.setTitle("Draw");
         stage.setScene(scene);
         stage.show();
